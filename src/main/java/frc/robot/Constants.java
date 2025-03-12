0f1a99e6494eb89d3061b45814fe2ccc7928c89e
SamJinLi <lijinzesamuel@gmail.com>
2025-03-11 19:59:59 -0700

    Elevator & wrist done!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -383,19 +383,32 @@ public final class Constants {
         public static final IdleMode wristIdleMode = IdleMode.kBrake;
         
         public static final boolean encoderInverted = false;
-        public static final int stallLimit = 10;
-        public static final int freeLimit = 10;
+        /*on branch tune_lower_intake PID&FF start (not really tuned) */
+        // public static final int stallLimit = 10;
+        // public static final int freeLimit = 10;
         
-        public static final double kP = 0.00, kI = 0, kD = 0, izone = 2, tolerance = .5;
-        public static final double kS = 0, kG = .19, kV = .53, kA = 0;
-        public static final double allowedClosedLoopError = 0.5;
-        public static final double maxAcceleration = 720, maxVelocity = 360;//Accelaration is in units of RPM per Second (RPM/s) & Maximum Velocity is in units of Revolutions per Minute (RPM)
-        public static final double wristMaxDegrees = 87, wristMinDegrees = -144;
-
+        // public static final double kP = 0.00, kI = 0, kD = 0, izone = 2, tolerance = .5;
+        // public static final double kS = 0, kG = .19, kV = .53, kA = 0;
+        // public static final double allowedClosedLoopError = 0.5;
+        // public static final double maxAcceleration = 720, maxVelocity = 360;//Accelaration is in units of RPM per Second (RPM/s) & Maximum Velocity is in units of Revolutions per Minute (RPM)
+        // public static final double wristMaxDegrees = 87, wristMinDegrees = -144;
+        /*on branch tune_lower_intake PID&FF end (not really tuned) */
         public static final double L2 = -48.8018;
         public static final double L3 = -46.2412;
         public static final double L4 = -8;
         public static final double coralStation = 35.81813;
+
+
+        /*on branch scrimage v2 PID&FF start (not really tuned) */
+        public static final int stallLimit = 5;
+        public static final int freeLimit = 20;
+        
+        public static final double kP = 0.08, kI = 0, kD = 0, izone = 2, tolerance = 1.0;
+        public static final double kS = 0, kG = .25, kV = 0, kA = 0;
+        public static final double allowedClosedLoopError = 0.5;
+        public static final double maxAcceleration = 5000, maxVelocity = 10000;//Accelaration is in units of RPM per Second (RPM/s) & Maximum Velocity is in units of Revolutions per Minute (RPM)
+        public static final double wristMaxDegrees = 87, wristMinDegrees = -144;
+        /*on branch scrimage v2 PID&FF end (not really tuned) */
     }
     public static final class ElevatorConstants{
         //TODO: TUNE ALL THESE VALUES
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.ElevatorSubsystem;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -24,8 +23,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static final CTREConfigs ctreConfigs = new CTREConfigs();
-
-  private ElevatorSubsystem elevatorSubsystem = ElevatorSubsystem.getInstance();
 
   private final RobotContainer m_robotContainer;
 
