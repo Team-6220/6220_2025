a8f5e00c67ef9605227149aed666e95b3ccf90c2
Mason Lee <mason101488@gmail.com>
2025-09-12 17:17:55 -0700

    Create TOFSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/TOFSubsystem.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANrange;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TOFSubsystem extends SubsystemBase {
+  public final static int lowerleftRangeID=22;
+  public final static int lowerrightRangeID=23;
+  public final static int topRangeID=21;
+  public static CANrange lowerleft;
+  public static CANrange lowerright;
+  public static CANrange top;
+  public TOFSubsystem() {
+    lowerleft = new CANrange(lowerleftRangeID);
+    lowerright = new CANrange(lowerrightRangeID);
+    top = new CANrange(topRangeID);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("lowerleftCanrange", lowerleft.getDistance().getValueAsDouble());
+    SmartDashboard.putNumber("lowerrightCanrange", lowerright.getDistance().getValueAsDouble());
+    SmartDashboard.putNumber("topCanrange", top.getDistance().getValueAsDouble());
+  }
+  public double getLowerDistance(){
+    return 0.0;
+  }
+  public double getElevatorDistance(){
+    return 0.0;
+  }
+}
